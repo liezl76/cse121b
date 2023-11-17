9f36f0e3de3d4f96459063e03b91450597f064ec
liezl76 <liezl1106@gmail.com>
2023-11-17 16:30:42 +0800

    add code

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -4,31 +4,35 @@
 /* Function Definition - Add Numbers */
 function add(number1,number2){
     return number1 + number2;
-};
+}
 
 function addNumber(){
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
     document.getElementById('sum').value = add(addNumber1, addNumber2);
-};
+}
 
 document.querySelector('#addNumbers').addEventListener("click",addNumber);
 
 /* Function Expression - Subtract Numbers */
-function subtract(number1,number2){
+function subtractNumbers(number1,number2){
     return number1 - number2;
-};
+}
 
-function subtract(){
+function handleSubtractClick(){
     let subtractNumber1 = Number(document.querySelector('#subtract1').value);
     let subtractNumber2 = Number(document.querySelector('#subtract2').value);
-    document.getElementById('difference').value = subtract(subtractNumber1, subtractNumber2);
-};
+    document.getElementById('difference').value = subtractNumbers(subtractNumber1, subtractNumber2);
+}
 
-document.querySelector('#subtractNumbers').addEventListener("click",subtractNumber);
+document.querySelector('#subtractNumbers').addEventListener("click",handleSubtractClick);
 
 /* Arrow Function - Multiply Numbers */
+function multiplyNumbers(number1,number2){
+    return number1 * number2;
+}
 
+function multiplyNumbers
 
 /* Open Function Use - Divide Numbers */
 
