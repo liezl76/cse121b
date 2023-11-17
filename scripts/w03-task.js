7cb2ca26150c8c73b14ed22300c12693d762274a
liezl76 <liezl1106@gmail.com>
2023-11-17 16:12:58 +0800

    edit w03-task.js

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -2,10 +2,30 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
+function add(number1,number2){
+    return number1 + number2;
+};
 
+function addNumber(){
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    document.getElementById('sum').value = add(addNumber1, addNumber2);
+};
+
+document.querySelector('#addNumbers').addEventListener("click",addNumber);
 
 /* Function Expression - Subtract Numbers */
+function subtract(number1,number2){
+    return number1 - number2;
+};
 
+function subtract(){
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.getElementById('difference').value = subtract(subtractNumber1, subtractNumber2);
+};
+
+document.querySelector('#subtractNumbers').addEventListener("click",subtractNumber);
 
 /* Arrow Function - Multiply Numbers */
 
