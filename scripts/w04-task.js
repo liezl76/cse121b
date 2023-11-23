dc32337302ce177a01e82e39ee1e38068a88e14f
liezl76 <liezl1106@gmail.com>
2023-11-23 16:49:15 +0800

    edit code

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -35,7 +35,6 @@ document.querySelector('#name').textContent = myProfile.name;
 document.querySelector('#photo').src = myProfile.photo;
 document.querySelector('#name').alt = myProfile.name;
 /* Favorite Foods List*/
-let favoriteFoods = document.getElementById('favorite-foods');
 myProfile.favoriteFoods.forEach(food=> {
     let li = document.createElement('li');
     li.textContent = food;
@@ -43,15 +42,13 @@ myProfile.favoriteFoods.forEach(food=> {
 });
 
 /* Hobbies List */
-let hobbiesList = document.getElementById('hobbies');
 myProfile.hobbies.forEach(hobby => {
   let li = document.createElement('li');
   li.textContent = hobby;
-  hobbiesList.appendChild(li);
+  document.querySelector('#hobbies').appendChild(li);
 });
 
 /* Places Lived DataList */
-let placesLivedList = document.getElementById('places-lived');
 myProfile.placesLived.forEach(place => {
   let dt = document.createElement('dt');
   dt.textContent = place.place;
@@ -59,7 +56,7 @@ myProfile.placesLived.forEach(place => {
   let dd = document.createElement('dd');
   dd.textContent = place.length;
 
-  placesLivedList.appendChild(dt);
-  placesLivedList.appendChild(dd);
+  document.querySelector('#places-lived').appendChild(dt);
+  document.querySelector('#places-lived').appendChild(dd);
 });
 
