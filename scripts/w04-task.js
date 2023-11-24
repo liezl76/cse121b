e125ef827da3dd8f55899dbbc932ee72a734502f
liezl76 <liezl1106@gmail.com>
2023-11-24 09:35:13 +0800

    edit dt and dd

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -10,7 +10,7 @@ let myProfile = {
     'Ice Cream',
     'Spaghetti'
   ],
-  hobbies: ['Listening to Podcast', 'Reading', 'Driving'],
+  hobbies: ['Listening to Podcast', 'Reading', 'Driving Motorcycle'],
   placesLived: []
 };
 
@@ -18,13 +18,13 @@ let myProfile = {
 myProfile.placesLived.push(
   {
     place: 'San Miguel, Jordan, Guimaras, Philippines',
-    lenght: '42 years'
+    length: '42 years'
   }  
 );
 myProfile.placesLived.push(
     {
       place: 'San Roque, Nueva Valencia, Guimaras, Philippines',
-      lenght: '3 years'
+      length: '3 years'
     }  
   );
 
@@ -52,7 +52,6 @@ myProfile.hobbies.forEach(hobby => {
 myProfile.placesLived.forEach(place => {
   let dt = document.createElement('dt');
   dt.textContent = place.place;
-
   let dd = document.createElement('dd');
   dd.textContent = place.length;
 
