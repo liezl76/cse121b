088603bb56b2d63a548f6c13b64aa7092ddb75f7
liezl76 <liezl1106@gmail.com>
2023-12-05 15:49:07 +0800

    edit code in hms script

--- scripts/hms.js
+++ scripts/hms.js
@@ -1,25 +1,128 @@
 let patients = [];
+let appointments = [];
 
 function addPatient() {
   const patientName = document.getElementById('patientName').value;
   const patientAge = document.getElementById('patientAge').value;
   const patientCondition = document.getElementById('patientCondition').value;
-
+  const appointmentDate = document.getElementById('appointmentDate').value;
+  const appointmentTime = document.getElementById('appointmentTime').value;
+  
   const newPatient = {
-    id: generatePatientId(),
     name: patientName,
     age: patientAge,
     condition: patientCondition,
   };
 
-  patients.push(newPatient);
-  displayPatients();
-  clearForm();
+  // Save the new patient to the GitHub Gist API
+  savePatientToAPI(newPatient)
+    .then(() => {
+      // After successfully saving to API, update the local data
+      patients.push(newPatient);
+      displayPatients();
+      clearForm();
+    })
+    .catch(error => {
+      console.error('Error saving patient to API:', error);
+      alert('Failed to save patient data. Please try again.');
+    });
+}
+  //Schedule an appointment for the patient
+  const newAppointment = {
+    patientName: patientName,
+    date: appointmentDate,
+    time: appointmentTime,
+  };
+
+  appointments.push(newAppointment);
+  displayAppointments();
+
+  function displayAppointments() {
+    const appointmentsList = document.getElementById('appointmentsList');
+  
+    // Clear the existing list
+    appointmentsList.innerHTML = '';
+  
+    appointments.forEach(appointment => {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <strong>${appointment.patientName}</strong> - Date: ${appointment.date}, Time: ${appointment.time}
+      `;
+      appointmentsList.appendChild(listItem);
+    });
+  }
+
+function savePatientToAPI(patient) {
+  // Replace 'YOUR_GIST_ID' with the actual Gist ID where you want to store patient data
+  const gistId = 'YOUR_GIST_ID';
+  const apiUrl = `https://api.github.com/gists/${gistId}`;
+
+  return fetch(apiUrl, {
+    method: 'PATCH', // Use PATCH to update an existing Gist
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer YOUR_GITHUB_TOKEN', // Replace with your GitHub token
+    },
+    body: JSON.stringify({
+      files: {
+        'patients.json': {
+          content: JSON.stringify([...patients, patient]),
+        },
+      },
+    }),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+  });
 }
 
-function generatePatientId() {
-  // You can implement a proper ID generation logic here
-  return Math.random().toString(36).substring(7);
+function deletePatient(index) {
+  const confirmed = confirm('Are you sure you want to delete this patient?');
+
+  if (confirmed) {
+    // Delete the patient from the GitHub Gist API
+    deletePatientFromAPI(index)
+      .then(() => {
+        // After successfully deleting from API, update the local data
+        patients.splice(index, 1);
+        displayPatients();
+      })
+      .catch(error => {
+        console.error('Error deleting patient from API:', error);
+        alert('Failed to delete patient data. Please try again.');
+      });
+  }
+}
+
+function deletePatientFromAPI(index) {
+  const gistId = 'YOUR_GIST_ID';
+  const apiUrl = `https://api.github.com/gists/${gistId}`;
+
+  return fetch(apiUrl, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer YOUR_GITHUB_TOKEN',
+    },
+    body: JSON.stringify({
+      files: {
+        'patients.json': {
+          content: JSON.stringify([...patients.slice(0, index), ...patients.slice(index + 1)]),
+        },
+      },
+    }),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+  });
+}
+
+function clearForm() {
+  document.getElementById('patientForm').reset();
 }
 
 function displayPatients() {
@@ -28,109 +131,42 @@ function displayPatients() {
   // Clear the existing list
   patientsList.innerHTML = '';
 
-  patients.forEach(patient => {
+  patients.forEach((patient, index) => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `
       <strong>${patient.name}</strong> (Age: ${patient.age}, Condition: ${patient.condition})
-      <button onclick="updatePatient('${patient.id}')">Update</button>
-      <button onclick="deletePatient('${patient.id}')">Delete</button>
+      <button onclick="deletePatient(${index})">Delete</button>
     `;
     patientsList.appendChild(listItem);
   });
 }
 
-function updatePatient(id) {
-  const updatedInfo = prompt('Enter updated information for the patient:');
-  const patientIndex = patients.findIndex(patient => patient.id === id);
+// Load patients from the GitHub Gist API on page load
+document.addEventListener('DOMContentLoaded', () => {
+  loadPatientsFromAPI();
+});
 
-  if (patientIndex !== -1) {
-    patients[patientIndex].condition = updatedInfo;
-    displayPatients();
-  }
+function loadPatientsFromAPI() {
+  const gistId = 'YOUR_GIST_ID';
+  const apiUrl = `https://api.github.com/gists/${gistId}`;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Assuming the patients data is stored in a file named 'patients.json'
+      const patientsFile = data.files['patients.json'];
+      if (patientsFile) {
+        patients = JSON.parse(patientsFile.content);
+        displayPatients();
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching patient data from API:', error);
+      alert('Failed to load patient data. Please try again.');
+    });
 }
-
-function deletePatient(id) {
-  const confirmed = confirm('Are you sure you want to delete this patient?');
-
-  if (confirmed) {
-    patients = patients.filter(patient => patient.id !== id);
-    displayPatients();
-  }
-}
-
-function clearForm() {
-  document.getElementById('patientForm').reset();
-}
-
-// Function to save patients to local storage
-function savePatientsToLocalStorage() {
-  localStorage.setItem('patients', JSON.stringify(patients));
-}
-
-// Function to load patients from local storage
-function loadPatientsFromLocalStorage() {
-  const storedPatients = localStorage.getItem('patients');
-  if (storedPatients) {
-    patients = JSON.parse(storedPatients);
-  }
-}
-
-function displayAppointments(appointments) {
-  const appointmentsList = document.getElementById('appointmentsList');
-
-  // Clear the existing list
-  appointmentsList.innerHTML = '';
-
-  appointments.forEach(appointment => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <strong>${appointment.patientName}</strong> - Date: ${appointment.date}, Time: ${appointment.time}
-    `;
-    appointmentsList.appendChild(listItem);
-  });
-}
-
-//
-  document.addEventListener('DOMContentLoaded', () => {
-    // Load patients from local storage
-    loadPatientsFromLocalStorage();
-
-    // Fetch patient data from an external API (replace with your API endpoint)
-    fetch('https://github.com/liezl76/cse121b/blob/main/resources/patients.json')
-      .then(response => response.json())
-      .then(data => {
-        // Assuming the API response is an array of patients
-        displayPatients(data);
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  });
-  
-  function displayPatients(patients) {
-    const patientListContainer = document.getElementById('patientList');
-  
-    // Use template literals and array method for data manipulation
-    const patientListHTML = patients.map(patient => `
-      <div class="patientCard">
-        <h2>${patient.name}</h2>
-        <p>Age: ${patient.age}</p>
-        <p>Condition: ${patient.condition}</p>
-      </div>
-    `).join('');
-  
-    // Display the patient list in the DOM
-    patientListContainer.innerHTML = patientListHTML;
-  
-    // Example of conditional branching
-    if (patients.length === 0) {
-      patientListContainer.innerHTML = '<p>No patients available.</p>';
-    }
-
-    // Function to add a patient
-    function addPatient() {
-    // Your existing addPatient function...
-
-    // Save patients to local storage after adding a patient
-    savePatientsToLocalStorage();
-    }
-}
-  
